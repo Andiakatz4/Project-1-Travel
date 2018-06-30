36b77caca9a02812b4c280dce3758175b5b2d4ec
Elizaveta Onatsko <elizavetaonatsko@Elizavetas-MacBook-Pro.local>
2018-06-30 14:40:05 -0700

    finished on click

--- assets/firstPage.js
+++ assets/firstPage.js
@@ -8,20 +8,16 @@
   };
 
   var database = fire.datebase(); */
-  var myIndex = 0;
-  carousel();
-  
-  function carousel() {
-      var i;
-      var x = document.getElementsByClassName("mySlides");
-      for (i = 0; i < x.length; i++) {
-         x[i].style.display = "none";  
-      }
-      myIndex++;
-      if (myIndex > x.length) {myIndex = 1}    
-      x[myIndex-1].style.display = "block";  
-      setTimeout(carousel, 5000); // Change image every 2 seconds
-  }
 
-  
-  
+  $(".label").on("click", function(e) {
+    console.log("hi");
+    e.preventDefault();
+    if($("#search-box").val() > "")
+    window.location.href="city-info.html";
+    var name = $("#search-box").val().trim();
+    localStorage.name = name;
+
+  });
+  function loadTitle(){
+    $(".secondTitle").append(localStorage.name);
+  }
